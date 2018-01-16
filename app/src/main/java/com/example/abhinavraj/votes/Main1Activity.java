721755dc2fe4230c7ef407f88e4c4b3bbe10dafa
Harsh Patel <32356267+harsh-2711@users.noreply.github.com>
2018-01-16 20:10:58 +0530

    Updated Main1Activity
    On the main screen, I have added to separate logins for the voter as well as host. Also, I have added a username section to preview distinction among user.

--- app/src/main/java/com/example/abhinavraj/votes/Main1Activity.java
+++ app/src/main/java/com/example/abhinavraj/votes/Main1Activity.java
@@ -3,33 +3,83 @@ package com.example.abhinavraj.votes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main1Activity extends AppCompatActivity {
-    EditText ed;
+    EditText ed,txt;
     String s="RENDER";
+    String str = "";
     Button proceed;
+    TextView b1,b2;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        b1 = (TextView) findViewById(R.id.toVote);
+        b2 = (TextView) findViewById(R.id.HostVote);
+        b1.setText(Html.fromHtml("<u>VOTER LOGIN</u>"));
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(counter%2 == 0)
+                counter++;
+                if(counter%2 != 0)
+                {
+                    b2.setText(Html.fromHtml("<u>HOST LOGIN</u>"));
+                    b1.setText("VOTER LOGIN");
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(counter%2 != 0)
+                    counter--;
+                if(counter%2 == 0)
+                {
+                    b1.setText(Html.fromHtml("<u>VOTER LOGIN</u>"));
+                    b2.setText("HOST LOGIN");
+                }
+            }
+        });
+
         proceed= (Button) findViewById(R.id.button);
         proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                      ed = (EditText) findViewById(R.id.editText3);
-                    if (ed.getText().toString().equals(s)) {
-                        Intent i = new Intent(Main1Activity.this, MainActivity.class);
-                        startActivity(i);
-                    }
-                    else {
-                        Toast.makeText(Main1Activity.this, "Incorrect password", Toast.LENGTH_LONG).show();
-                    }
-                 ed.getText().clear();
+                     txt = (EditText) findViewById(R.id.editText6);
+                     if(txt.getText().toString().equals(str)){
+                         Toast.makeText(Main1Activity.this, "Enter Username", Toast.LENGTH_SHORT).show();
+                     }
+                     else{
+
+                         if(ed.getText().toString().equals(s)){
+                             if(counter%2 == 0)
+                             {
+                                 Intent i = new Intent(Main1Activity.this, UserActivity.class);
+                                 startActivity(i);
+                             }
+                             else
+                             {
+                                 Intent i = new Intent(Main1Activity.this, MainActivity.class);
+                                 startActivity(i);
+                             }
+                         }
+                         else
+                         {
+                             Toast.makeText(Main1Activity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                         }
+                         ed.getText().clear();
+                     }
             }
         });
 
